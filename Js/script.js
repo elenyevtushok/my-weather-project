7019eca59aa2b94416050b75b01605d96705e088
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-08-15 10:02:20 +0200

    Add the change of main icon

--- Js/script.js
+++ Js/script.js
@@ -67,6 +67,10 @@ function showTemperature(response) {
 	windElement.innerHTML = `Wind: ${wind} km/h`;
 	let dateElement = document.querySelector("#date");
 	dateElement.innerHTML = formatDate(response.data.dt * 1000);
+	let iconElement = document.querySelector("#icon");
+	iconElement.setAttribute ("src",
+		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+	iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function fetchWeatherByCity(city) {
