32cd190d71891935c24d98d451c8bb8610486b37
elenyevtushok <60004031+elenyevtushok@users.noreply.github.com>
2022-08-16 18:39:38 +0200

    Add API

--- Js/script.js
+++ Js/script.js
@@ -24,11 +24,52 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
 	let date = new Date(timestamp * 1000);
 	let day = date.getDay();
-	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	let days = ["Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",];
 
 	return days[day];
 }
+function formatDailyForecastDayMonth(timestamp) {
+	let date = new Date(timestamp * 1000);
+	let day = date.getDate();
+	if (day < 10) {
+		day = `0${day}`;
+	}
+	let month = date.getMonth() + 1;
+	if (month < 10) {
+		month = `0${month}`;
+	}
+	let DayMonth = `${day}.${month}`;
+	return DayMonth;
+}
 
+function displayForecast(response) {
+	let forecast = response.data.daily;
+	let forecastElement = document.querySelector("#forecast");
+	let forecastHTML = `<div class="col">`;
+	forecast.forEach(function(forecastDay, index){
+		if (index < 6 && index > 0){
+		forecastHTML = forecastHTML + `<div class="row-4 weather-forecast-block">
+						<div class="col-md-4 weather-forecast-day">${formatDay(forecastDay.dt)}</div>
+						<div class="col-md-3 weather-forecast-date">${formatDailyForecastDayMonth(forecastDay.dt)}</div>
+						<div class="col-md-3 weather-forecast-icon">
+							<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="60">
+						</div>
+						<div class="col-md-2 weather-forecast-temperatures">
+							<span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°•</span>
+							<span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+						</div>
+					</div>`;
+		}
+	});
+	forecastHTML = forecastHTML + `</div>`;				
+	forecastElement.innerHTML = forecastHTML;
+}
 
 //==============================================================
 function searchCity(event) {
@@ -49,7 +90,12 @@ form.addEventListener("submit", searchCity);
 document.querySelector("#buttonCurrentLocation").addEventListener("click", updateWeatherByCurrentLocation);
 
 //==============================================================
+let apiKey = "847c95a73fdbcb2728943e12f70e3bee";
 
+function getForecast(coordinates){
+	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
 	celsiusTemperature = response.data.main.temp;
@@ -73,17 +119,19 @@ function showTemperature(response) {
 	iconElement.setAttribute ("src",
 		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 	iconElement.setAttribute("alt", response.data.weather[0].description);
+
+	getForecast(response.data.coord);
 }
 
 function fetchWeatherByCity(city) {
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=847c95a73fdbcb2728943e12f70e3bee`;
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 	axios.get(apiUrl).then(showTemperature);
 }
 
 function fetchWeatherByCoordinate(position) {
 	let lat = position.coords.latitude;
 	let lon = position.coords.longitude;
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=847c95a73fdbcb2728943e12f70e3bee`;
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 	axios.get(apiUrl).then(showTemperature);
 }
 
@@ -107,6 +155,8 @@ function displayCelsiusTemperature(event) {
 }
 let celsiusTemperature = null;
 
+
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
@@ -114,5 +164,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 fetchWeatherByCity("Valencia");
-
 
